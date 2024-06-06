4e14ab5774979180d6404d3d177196a5a3416e78
lysoputhea <lysoputhea007@gmail.com>
2024-06-06 10:18:55 +0700

    First upload

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+/**
+ * router/index.ts
+ *
+ * Automatic routes for `./src/pages/*.vue`
+ */
+
+// Composables
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { setupLayouts } from 'virtual:generated-layouts'
+
+
+// const routes = [
+//   {
+//     path: '/',
+//     component: DashboardLayout,
+//     children: [
+//       {
+//         path: '',
+//         name: 'DashboardHome',
+//         component: DashboardHome,
+//       },
+//       {
+//         path: 'users',
+//         name: 'UserManagement',
+//         component: UserManagement,
+//       },
+//     ],
+//   },
+// ];
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  extendRoutes: setupLayouts,
+})
+
+export default router
